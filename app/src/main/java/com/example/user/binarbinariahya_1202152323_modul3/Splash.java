fdc5eace013b3ce7949a62edb3117c9e06473f47
binabinar <binariahya@gmail.com>
2018-02-24 21:48:56 +0700

    modul 3

---
+++ app/src/main/java/com/example/user/binarbinariahya_1202152323_modul3/Splash.java
@@ -0,0 +1,34 @@
+package com.example.user.binarbinariahya_1202152323_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class Splash extends AppCompatActivity {
+    private static int splashInterval = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//command ini digunakan untuk membuat layar aplikasi menjadi full screen//
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent a = new Intent(Splash.this, Login.class);
+                startActivity(a);
+                Toast.makeText(Splash.this, "Selamat Datang", Toast.LENGTH_LONG).show();
+                this.finish();//untuk menampilkan pop up tulisan yang kita inginkan//
+            }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
+    }
+}
---
+++ app/src/main/java/com/example/user/binarbinariahya_1202152323_modul3/pilihAir.java
@@ -0,0 +1,29 @@
+package com.example.user.binarbinariahya_1202152323_modul3;
+
+/**
+ * Created by USER on 24/02/2018.
+ */
+
+public class pilihAir {
+    int foto;
+    String nama;
+    String detail;
+
+    public pilihAir(int foto, String nama, String detail) {
+        this.foto = foto;
+        this.nama = nama;
+        this.detail = detail;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getDetail() {return detail; }
+
+}
+
